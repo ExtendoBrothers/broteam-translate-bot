92e7151950860c9795d3da2ad45713253f878bbd
ExtendoBrothers <github@mail.pirat3.eu>
2025-12-13 10:06:18 +0100

    Remove unused files: test-franc.js, test RSS files, and start-bot.bat

--- start-bot.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting BroTeam Translate Bot...
-pm2 start ecosystem.config.js
-echo Bot started. Use 'pm2 logs broteam-translate-bot' to view logs.
-pause
--- test-franc.js
+++
@@ -1,7 +0,0 @@
-const franc = require('franc');
-
-const text = 'वुडपॉट';
-
-const lang = franc.franc(text, { minLength: 3 });
-
-console.log(`Detected language for '${text}': ${lang}`);
